595113012c24c28a6ac7cd46d2017374474c9d1c
PascalBreno <brenodml@live.com>
2019-11-03 05:11:06 -0300

    Adiciona palavras sobre todos os documentos

--- main.py
+++ main.py
@@ -17,12 +17,18 @@ def includeWordsOfDocuments():
 Documents  = includeWordsOfDocuments()
 DicWords = []
 DicWord = {}
+DicWordAllDocument = {}
+# Check if exist word in Document actual and All Documents
 for words in Documents:
     for word in words:
         if word in DicWord:
             DicWord[word] += 1
         else:
             DicWord[word] = 1
+        if word in DicWordAllDocument:
+            DicWordAllDocument[word]+=1
+        else:
+            DicWordAllDocument[word]=1
     DicWords.append(DicWord.copy())
     DicWord.clear()
 for index, Document in enumerate(DicWords):
@@ -30,4 +36,10 @@ for index, Document in enumerate(DicWords):
     print("Document: ", index+1)
     for key, value in Document.items():
         print(key, "=>", value)
-# Check if the word exist in Dic
+
+#Here print all words in all Documents
+print("All Document: ")
+for key, value in DicWordAllDocument.items():
+    print(key, "=>", value)
+
+#TODO get a number of word in N Documents and calculate LN(Number of all Documents / Number of Documents with the word W). This is your IDF
