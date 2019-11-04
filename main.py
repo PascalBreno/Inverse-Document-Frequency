c4880d0df3aea3140ae78e423681b1ef3d42eced
PascalBreno <brenodml@live.com>
2019-11-04 03:40:37 -0300

    Adiciona o calculo para as palavras

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # TF-IDF é uma medida estatística que indica a importância de uma palavra  TF - IDF = TF*IDF
 # TF = Numero de vezes que uma palavra ocorre no documento / Número total de palavras no documento
+import math
 
-text1 = "It is going to rain today to rain"
-text2 = "Today I am not going outside"
+text1 = "It is going to rain today"
+text2 = "today I am not going outside"
 text3 = "I am going to watch the season premiere"
 
 
@@ -14,7 +15,33 @@ def includeWordsOfDocuments():
     Words.append(text3.split(" "))
     return Words
 
-Documents  = includeWordsOfDocuments()
+
+def printAllDocument():
+    for index, Document in enumerate(DicWords):
+        # The index is the number of Document
+        print("Document: ", index + 1)
+        for key, value in Document.items():
+            print(key, "=>", value)
+
+    # Here print all words in all Documents
+    print("All Document: ")
+    for key, value in DicWordAllDocument.items():
+        print(key, "=>", value)
+def setTFinAllWords():
+    for Document in DicWords:
+        tam = 0
+        for word in Document:
+            if Document.get(word,"")>0:
+                tam +=1
+        for word in Document:
+            aux = Document.get(word,"")
+            Document[word] = float(aux/tam)
+def setTF():
+    for Document in DicWords:
+        for word in Document:
+            Document[word] = Document.get(word,"")*NumberWordDocument.get(word, "")
+    printAllDocument()
+Documents = includeWordsOfDocuments()
 DicWords = []
 DicWord = {}
 DicWordAllDocument = {}
@@ -26,20 +53,29 @@ for words in Documents:
         else:
             DicWord[word] = 1
         if word in DicWordAllDocument:
-            DicWordAllDocument[word]+=1
+            DicWordAllDocument[word] += 1
         else:
-            DicWordAllDocument[word]=1
+            DicWordAllDocument[word] = 1
     DicWords.append(DicWord.copy())
     DicWord.clear()
+# Add all words in all Documents. If the Document dont have the Word, so add 0 to your value
+NumberWordDocument = {}
 for index, Document in enumerate(DicWords):
-    #The index is the number of Document
-    print("Document: ", index+1)
-    for key, value in Document.items():
-        print(key, "=>", value)
+    for word in DicWordAllDocument:
+        if word in Document and Document.get(word, "") > 0:
+            if word in NumberWordDocument:
+                if not (NumberWordDocument[str(word)] > index):
+                    NumberWordDocument[str(word)] += 1
+            else:
+                NumberWordDocument[str(word)] = 1
+        else:
+            Document[word] = 0
 
-#Here print all words in all Documents
-print("All Document: ")
-for key, value in DicWordAllDocument.items():
-    print(key, "=>", value)
+setTFinAllWords()
+printAllDocument()
 
-#TODO get a number of word in N Documents and calculate LN(Number of all Documents / Number of Documents with the word W). This is your IDF
+NumberOfDocuments = len(DicWords)
+for word in NumberWordDocument:
+    x = NumberOfDocuments/NumberWordDocument.get(word," ")
+    NumberWordDocument[word] = math.log(x)
+setTF()
