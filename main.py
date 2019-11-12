20672a862ef187f270e146c20bfdaf2773a6d1c2
PascalBreno <brenodml@live.com>
2019-11-11 23:01:49 -0300

    Finaliza o TD para inicalizar a clusterização

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import math
 text1 = "It is going to rain today"
 text2 = "today I am not going outside"
 text3 = "I am going to watch the season premiere"
+DicWords = []
+DicWord = {}
+DicWordAllDocument = {}
 
 
-def includeWordsOfDocuments():
+def includeWordsOfDocuments(Document):
     # TODO here need to add read in all Documents e add e return the number Documents
     Words = []
-    Words.append(text1.split(" "))
-    Words.append(text2.split(" "))
-    Words.append(text3.split(" "))
+    for x in Document:
+        Words.append(x.split(" "))
     return Words
 
 
@@ -24,58 +26,64 @@ def printAllDocument():
             print(key, "=>", value)
 
     # Here print all words in all Documents
-    print("All Document: ")
-    for key, value in DicWordAllDocument.items():
-        print(key, "=>", value)
+    # print("All Document: ")
+    # for key, value in DicWordAllDocument.items():
+    #    print(key, "=>", value)
+
+
 def setTFinAllWords():
     for Document in DicWords:
         tam = 0
         for word in Document:
-            if Document.get(word,"")>0:
-                tam +=1
+            if Document.get(word, "") > 0:
+                tam += 1
         for word in Document:
-            aux = Document.get(word,"")
-            Document[word] = float(aux/tam)
-def setTF():
+            aux = Document.get(word, "")
+            Document[word] = float(aux / tam)
+
+
+def setTF(NumberWordDocument):
     for Document in DicWords:
         for word in Document:
-            Document[word] = Document.get(word,"")*NumberWordDocument.get(word, "")
-    printAllDocument()
-Documents = includeWordsOfDocuments()
-DicWords = []
-DicWord = {}
-DicWordAllDocument = {}
-# Check if exist word in Document actual and All Documents
-for words in Documents:
-    for word in words:
-        if word in DicWord:
-            DicWord[word] += 1
-        else:
-            DicWord[word] = 1
-        if word in DicWordAllDocument:
-            DicWordAllDocument[word] += 1
-        else:
-            DicWordAllDocument[word] = 1
-    DicWords.append(DicWord.copy())
-    DicWord.clear()
-# Add all words in all Documents. If the Document dont have the Word, so add 0 to your value
-NumberWordDocument = {}
-for index, Document in enumerate(DicWords):
-    for word in DicWordAllDocument:
-        if word in Document and Document.get(word, "") > 0:
-            if word in NumberWordDocument:
-                if not (NumberWordDocument[str(word)] > index):
-                    NumberWordDocument[str(word)] += 1
+            Document[word] = Document.get(word, "") * NumberWordDocument.get(word, "")
+
+
+def td(Documents2):
+    if type(Documents2) == list:
+        Documents = includeWordsOfDocuments(Documents2)
+    elif type(Documents2) == str:
+        Documents = []
+        Documents.append(Documents2)
+    # Check if exist word in Document actual and All Documents
+    for words in Documents:
+        for word in words:
+            if word in DicWord:
+                DicWord[word] += 1
             else:
-                NumberWordDocument[str(word)] = 1
-        else:
-            Document[word] = 0
+                DicWord[word] = 1
+            if word in DicWordAllDocument:
+                DicWordAllDocument[word] += 1
+            else:
+                DicWordAllDocument[word] = 1
+        DicWords.append(DicWord.copy())
+        DicWord.clear()
+    # Add all words in all Documents. If the Document dont have the Word, so add 0 to your value
+    NumberWordDocument = {}
+    for index, Document in enumerate(DicWords):
+        for word in DicWordAllDocument:
+            if word in Document and Document.get(word, "") > 0:
+                if word in NumberWordDocument:
+                    if not (NumberWordDocument[str(word)] > index):
+                        NumberWordDocument[str(word)] += 1
+                else:
+                    NumberWordDocument[str(word)] = 1
+            else:
+                Document[word] = 0
 
-setTFinAllWords()
-printAllDocument()
-
-NumberOfDocuments = len(DicWords)
-for word in NumberWordDocument:
-    x = NumberOfDocuments/NumberWordDocument.get(word," ")
-    NumberWordDocument[word] = math.log(x)
-setTF()
+    setTFinAllWords()
+    NumberOfDocuments = len(DicWords)
+    for word in NumberWordDocument:
+        x = NumberOfDocuments / NumberWordDocument.get(word, " ")
+        NumberWordDocument[word] = math.log(x)
+    setTF(NumberWordDocument)
+    return DicWords
---
+++ test.py
@@ -0,0 +1,11 @@
+import main
+
+text1 = "It is going to rain today"
+text2 = "today I am not going outside"
+text3 = "I am going to watch the season premiere"
+Words = []
+Words.append(text1)
+Words.append(text2)
+Words.append(text3)
+tdif = main.td(Words)
+print(tdif)
